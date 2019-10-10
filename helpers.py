a4b215202ba7d5f161dce30014a6d110b899ba66
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2019-10-10 11:15:59 +0200

    adam\'s metric added

--- helpers.py
+++ helpers.py
@@ -48,13 +48,10 @@ def plot_hists(data, name):
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
+def metric(preds, actuals):
+    preds = preds.reshape(-1)
+    actuals = actuals.reshape(-1)
+    assert preds.shape == actuals.shape
+    return 100 * np.linalg.norm((actuals - preds) / actuals) / np.sqrt(preds.shape[0])
         
         
